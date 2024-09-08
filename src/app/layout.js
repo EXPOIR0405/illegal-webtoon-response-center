05b10a975cefe9d5eb0ed22748cde1d9a4079ae4
minjung <rkdalswjd0405@gmail.com>
2024-09-09 00:57:44 +0900

    통계기능시도 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,29 +1,21 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 import { SpeedInsights } from "@vercel/speed-insights/next";
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: '불법 웹툰 대응 센터',
+  description: '불법 웹툰 사이트 대응 및 웹툰 작가 지원 센터',
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body>
+    <html lang="ko">
+      <body className={inter.className}>
         <SpeedInsights />
         {children}
       </body>
     </html>
   );
-}
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: '불법 웹툰 대응 센터',
-  description: '불법 웹툰 사이트 대응 및 웹툰 작가 지원 센터',
-}
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="ko">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
 }
