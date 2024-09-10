225b1cc620ec0dc4cba8ae0e2c240a8182a6b5dd
minjung <rkdalswjd0405@gmail.com>
2024-09-10 16:09:24 +0900

    cors 설정 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Next.js 구성 옵션을 여기에 추가하세요
-};
-
-export default nextConfig;
+export default {
+  async headers() {
+    return [
+      {
+        // 모든 API 경로에 대해 CORS 허용
+        source: '/api/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // 모든 도메인 허용
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,POST,PUT,DELETE,OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Authorization',
+          },
+        ],
+      },
+    ]
+  },
+}
