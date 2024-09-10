3d805da44f80e3925beea1b0cdee6d53d24912c1
minjung <rkdalswjd0405@gmail.com>
2024-09-10 15:17:30 +0900

    씁... 테스트

--- pages/api/mongodb.js
+++ pages/api/mongodb.js
@@ -1,21 +1,11 @@
-import { MongoClient } from 'mongodb';
-
-const uri = process.env.MONGODB_URI; // Vercel 환경 변수에서 URI 가져옴
-const client = new MongoClient(uri);
-
 export default async function handler(req, res) {
+  console.log("MongoDB URI:", process.env.MONGODB_URI);  // URI 출력
   try {
-    // MongoDB 연결 시도
     await client.connect();
-    
-    // 데이터베이스와 컬렉션 선택
     const database = client.db('Cluster0');
     const collection = database.collection('testCollection');
-
-    // 데이터 삽입 (테스트용)
-    const result = await collection.insertOne({ name: "Minjeong", age: 18 });
     
-    // 성공적으로 데이터 삽입 응답
+    const result = await collection.insertOne({ name: "Minjeong", age: 18 });
     res.status(200).json({ message: "데이터가 성공적으로 추가되었습니다!", result });
   } catch (error) {
     console.error("MongoDB 연결 실패:", error.message);  // 오류 메시지 출력
