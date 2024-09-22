35bc36bd9c68769c331bb23ae1c1b816a70fdccc
minjung <rkdalswjd0405@gmail.com>
2024-09-22 17:34:05 +0900

    구글 통계 추가

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +13,32 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
+      <head>
+        {/* Google Tag Manager script */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','G-TTB216N4KD);
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
+        {/* Google Tag Manager noscript */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=G-TTB216N4KD`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         {children}
         <SpeedInsights />
       </body>
